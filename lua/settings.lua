099f0700caf2fda98610054f7df385b15a1a1a1a
nikolai.franke@gmail.com <nikolai.franke@gmail.com>
2021-10-20 14:22:25 +0200

    Added persistant undo

--- lua/settings.lua
+++ lua/settings.lua
@@ -11,6 +11,7 @@ vim.o.showmode = false
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
+vim.o.undofile = true
 
 vim.cmd('set completeopt=menu,menuone,noselect')
 vim.g.gruvbox_flat_style = 'hard'
