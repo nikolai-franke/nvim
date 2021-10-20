7ad9fe2c7c0e7583b6e4562a6cb149401f8a2fbf
Nikolai Franke <nikolai.franke@gmail.com>
2021-10-20 15:16:33 +0200

    Replaced ':' with '<cmd>'

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,21 +10,21 @@ vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { nor
 vim.api.nvim_set_keymap('n', '<leader>fc', '<cmd>Telescope neoclip<cr>', { noremap = true })
 
 -- Tree mappings
-vim.api.nvim_set_keymap('n', '<leader>t', ':NvimTreeToggle<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>NvimTreeToggle<cr>', { noremap = true })
 
 -- Easier window related stuff
-vim.api.nvim_set_keymap('n', '<tab>', ':bnext<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<s-tab>', ':bprev<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>v', ':vs<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>h', ':split<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<tab>', '<cmd>bnext<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<s-tab>', '<cmd>bprev<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>v', '<cmd>vs<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>h', '<cmd>split<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>wj', '<c-w>j', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>wk', '<c-w>k', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>wh', '<c-w>h', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>wl', '<c-w>l', { noremap = true })
-vim.api.nvim_set_keymap('n', '<m-j>', ':resize -4<cr>', { noremap = true }) -- alt + hjkl to resize windows
-vim.api.nvim_set_keymap('n', '<m-k>', ':resize +4<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<m-h>', ':vertical-resize -4<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<m-l>', ':vertical-resize +4<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<m-j>', '<cmd>resize -4<cr>', { noremap = true }) -- alt + hjkl to resize windows
+vim.api.nvim_set_keymap('n', '<m-k>', '<cmd>resize +4<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<m-h>', '<cmd>vertical-resize -4<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<m-l>', '<cmd>vertical-resize +4<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>we', '<c-w>=', { noremap = true }) -- automatically resize splits
 
 -- Easier buffer closing and deleting
@@ -32,8 +32,8 @@ vim.api.nvim_set_keymap('n', '<leader>bq', '<cmd>bdelete<cr>', { noremap = true 
 vim.api.nvim_set_keymap('n', '<leader>bd', '<cmd>Bdelete<cr>', { noremap = true })
 
 -- Easier saving and quitting
-vim.api.nvim_set_keymap('n', '<leader>wq', ':q<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>s', ':up<cr>', { noremap = true }) -- I use update so file is only actually saved if there was a change
+vim.api.nvim_set_keymap('n', '<leader>wq', '<cmd>q<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>s', '<cmd>up<cr>', { noremap = true }) -- I use update so file is only actually saved if there was a change
 
 -- Easy CAPS
 vim.api.nvim_set_keymap('i', '<c-u>', '<esc>viwUi', { noremap = true })
@@ -45,10 +45,10 @@ vim.api.nvim_set_keymap('n', 'gpp', '<plug>ReplaceWithRegisterLine', { noremap =
 vim.api.nvim_set_keymap('x', 'gp', '<plug>ReplaceWithRegisterVisual', { noremap = false })
 
 -- Neoformat
-vim.api.nvim_set_keymap('n', '<leader>nf', ':Neoformat<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>nf', '<cmd>Neoformat<cr>', { noremap = true })
 
 -- Undotree
-vim.api.nvim_set_keymap('n', '<leader>ut', ':UndotreeToggle<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>ut', '<cmd>UndotreeToggle<cr>', { noremap = true })
 
 -- Escape out of terminals
 vim.api.nvim_set_keymap('t', '<esc>', '<c-\\><c-n>', { noremap = true })
